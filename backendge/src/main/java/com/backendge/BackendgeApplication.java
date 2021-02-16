37ff77d17fe6e0ac35a4f163dd657773ec025c64
maiconjian <maiconjiantelles@gmail.com>
2021-02-16 10:41:37 -0300

    primeiro commit

---
+++ backendge/src/main/java/com/backendge/BackendgeApplication.java
@@ -0,0 +1,13 @@
+package com.backendge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendgeApplication.class, args);
+	}
+
+}
---
+++ backendge/src/test/java/com/backendge/BackendgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
